8390b9c43700ec1f66f7e5eeec0c1d85e2beecbf
park <kunwook9826@hanyang.ac.kr>
2021-05-05 16:04:35 +0900

    2998

--- 2998.cpp
+++ 2998.cpp
@@ -1,14 +1,23 @@
-#include <iostream>
-using namespace std;
+	#include <iostream>
+	#include <string>
+	#include <cmath>
+	using namespace std;
 
-int main() {
-	string s;
-	cin >> s;
-	int ans = 0;
-	for (int i = 0; i < s.length(); i++) {
-		if (s[i] == '1') {
-			ans = ans + pow(2, s.length() - 1 - i);
+	int main() {
+		string s;
+		cin >> s;
+		int len = (s.length() + 2) / 3 * 3;
+		int bt = len - s.length();
+		char* new_s = new char[len];
+		for (int i = 0; i < bt; i++) {
+			new_s[i] = '0';
 		}
+		for (int i = bt; i < len; i++) {
+			new_s[i] = s[i-bt];
+		}
+		int group_s = len / 3;
+		for (int i = 0; i < group_s; i++) {
+			cout <<(new_s[i * 3] - '0') * 4 + (new_s[i * 3 + 1] - '0') * 2 + (new_s[i * 3 + 2] - '0');
+		}
+
 	}
-	cout << oct << ans;
-}
