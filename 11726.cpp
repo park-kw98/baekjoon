2aabf0d83bc6cb57230fff3965ddd8ed3ba0eeb9
park <kunwook9826@hanyang.ac.kr>
2021-05-31 20:48:59 +0900

    11726

---
+++ 11726.cpp
@@ -0,0 +1,22 @@
+//2 x n ≈∏¿œ∏µ
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> v;
+
+void d_n(int n) {
+	v.push_back((v[n - 1] + v[n - 2]) % 10007);
+}
+
+int main() {
+	int n;
+	cin >> n;
+	v.push_back(0);
+	v.push_back(1);
+	v.push_back(2);
+	for (int i = 3; i <= n; i++)
+		d_n(i);
+	cout << v[n];
+}
