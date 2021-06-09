893198d56c402db26552db613c5faddc154e67c3
park <kunwook9826@hanyang.ac.kr>
2021-06-09 23:18:49 +0900

    9498

---
+++ 9498.cpp
@@ -0,0 +1,23 @@
+//시험 성적
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	if (n >= 90 && n <= 100)
+		cout << "A";
+	else if (n >= 80 && n < 90)
+		cout << "B";
+	else if (n >= 70 && n < 80)
+		cout << "C";
+	else if (n >= 60 && n < 70)
+		cout << "D";
+	else
+		cout << "F";
+
+	return 0;
+}
